631e8ec62c0d1bd26f081220ce8fa5f42092653e
phusomnia <tuanhphu2004thcs@gmail.com>
2025-10-10 12:36:34 +0700

    feat: readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 * Tạo page bên trong features, mỗi trang tương ứng với một folder, muốn kiểu builder/a/b thì lồng folder vào nhau muốn gom thành nhóm dùng pattern ( )
 * Mỗi page có cấu trúc sau: index.astro, các component
 * Trong index chứa RootLayout (layout tổng để render tailwind) mọi component con render ở trong layout này
+* Lưu ý khi xây component có sử dụng hook state của react
+```
+<RootLayout>
+    <Section client:only="react"/>
+</RootLayout>
+```
+luôn đặt client:only="react" ở page đó 
--- src/features/build-cv/section/section.tsx
+++ src/features/build-cv/section/section.tsx
@@ -1,8 +1,54 @@
+import { Form, FormControl, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input"
+import { useState } from "react";
+
 export function Section()
 {
+    const [formValue, setFormValue] = useState({
+        FirstName: "",
+        LastName: "",
+        Address: "",
+        ImageProfile: "",
+        CreatedAt: ""
+    });
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) { 
+        const { id , value } = e.target;
+        console.log(id, value); 
+        setFormValue(prev => ({
+            ...prev,
+            [id]: value,
+        }));
+    }
+
     return <>
-        <div className="p-4 bg-amber-100">
-            Hola
+        <div>
+            <label htmlFor="FirstName">First Name</label>
+            <Input
+                id="LastName"
+                name="LastName"
+                placeholder="Họ"
+                value={formValue.LastName}
+                onChange={handleChange}
+            />
+
+            <label htmlFor="FirstName">First Name</label>
+            <Input
+                id="FirstName"
+                name="FirstName"
+                placeholder="Tên"
+                value={formValue.FirstName}
+                onChange={handleChange}
+            />
+
+            <label htmlFor="FirstName">First Name</label>
+            <Input
+                id="Address"
+                name="Address"
+                placeholder="Địa chỉ"
+                value={formValue.Address}
+                onChange={handleChange}
+            />
         </div>
     </>
 }
